2d30112e37b6b9920db1bce643c029b69dadc3c9
kimzini <rlawlsdl11111@gmail.com>
2024-09-24 21:48:09 +0900

    [feat] 로그인 성공 시 jwt 발급

--- src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
+++ src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.OAuthJWT.config;
 
+import com.example.OAuthJWT.jwt.JWTUtil;
+import com.example.OAuthJWT.oauth2.CustomSuccessHandler;
 import com.example.OAuthJWT.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
-    public SecurityConfig(CustomOAuth2UserService customAuth2UserService) {
-        this.customAuth2UserService = customAuth2UserService;
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
+
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
     }
 
     @Bean
@@ -36,7 +43,11 @@ public class SecurityConfig {
 
         //oauth2
         http
-                .oauth2Login((oauth2) -> oauth2.userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig.userService(customAuth2UserService))));
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                );
 
         //경로별 인가 작업
         http
